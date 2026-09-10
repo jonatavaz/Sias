7e651f64495cd1e177d18b6a1b11f5d13b455fdc
Jonata Vaz <151793246+jonatavaz@users.noreply.github.com>
2026-09-10 20:41:11 -0300

    chore: setup initial Spring configuration

---
+++ src/main/java/com/ong/sias/SiasApplication.java
@@ -0,0 +1,13 @@
+package com.ong.sias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ong/sias/SiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ong.sias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
